2d98b2157405696470684af0cf43894a16368a5c
Mileto Di Marco <mileto.di.marco@howest.be>
2020-11-10 16:21:21 +0100

    Adds Movie entity

---
+++ Wba.EfBasics.Domain/Entities/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wba.EfBasics.Domain.Entities
+{
+    public class Movie
+    {
+    
+    }
+}
---
+++ Wba.EfBasics.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wba.EfBasics.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
